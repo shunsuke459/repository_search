7decc99b87f825be0b6890b7b53cc8f49ab9e053
shunsuke459 <shunsuke459@gmail.com>
2023-10-24 21:27:35 +0900

    add: Repositoryクラスの作成

---
+++ lib/page/search/presenter/repository.freezed.dart
@@ -0,0 +1,337 @@
+// coverage:ignore-file
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: type=lint
+// ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides, invalid_annotation_target, unnecessary_question_mark
+
+part of 'repository.dart';
+
+// **************************************************************************
+// FreezedGenerator
+// **************************************************************************
+
+T _$identity<T>(T value) => value;
+
+final _privateConstructorUsedError = UnsupportedError(
+    'It seems like you constructed your class using `MyClass._()`. This constructor is only meant to be used by freezed and you are not supposed to need it nor use it.\nPlease check the documentation here for more information: https://github.com/rrousselGit/freezed#custom-getters-and-methods');
+
+Repository _$RepositoryFromJson(Map<String, dynamic> json) {
+  return _Repository.fromJson(json);
+}
+
+/// @nodoc
+mixin _$Repository {
+  int? get id => throw _privateConstructorUsedError;
+  String? get name => throw _privateConstructorUsedError;
+  String? get language => throw _privateConstructorUsedError;
+  String? get avatarUrl => throw _privateConstructorUsedError;
+  @JsonKey(name: 'stargazers_count')
+  int? get starCount => throw _privateConstructorUsedError;
+  @JsonKey(name: 'watchers_count')
+  int? get watchersCount => throw _privateConstructorUsedError;
+  @JsonKey(name: 'forks_count')
+  int? get forksCount => throw _privateConstructorUsedError;
+  @JsonKey(name: 'open_issue_count')
+  int? get openIssueCount => throw _privateConstructorUsedError;
+  @JsonKey(name: 'html_url')
+  String? get htmlUrl => throw _privateConstructorUsedError;
+
+  Map<String, dynamic> toJson() => throw _privateConstructorUsedError;
+  @JsonKey(ignore: true)
+  $RepositoryCopyWith<Repository> get copyWith =>
+      throw _privateConstructorUsedError;
+}
+
+/// @nodoc
+abstract class $RepositoryCopyWith<$Res> {
+  factory $RepositoryCopyWith(
+          Repository value, $Res Function(Repository) then) =
+      _$RepositoryCopyWithImpl<$Res, Repository>;
+  @useResult
+  $Res call(
+      {int? id,
+      String? name,
+      String? language,
+      String? avatarUrl,
+      @JsonKey(name: 'stargazers_count') int? starCount,
+      @JsonKey(name: 'watchers_count') int? watchersCount,
+      @JsonKey(name: 'forks_count') int? forksCount,
+      @JsonKey(name: 'open_issue_count') int? openIssueCount,
+      @JsonKey(name: 'html_url') String? htmlUrl});
+}
+
+/// @nodoc
+class _$RepositoryCopyWithImpl<$Res, $Val extends Repository>
+    implements $RepositoryCopyWith<$Res> {
+  _$RepositoryCopyWithImpl(this._value, this._then);
+
+  // ignore: unused_field
+  final $Val _value;
+  // ignore: unused_field
+  final $Res Function($Val) _then;
+
+  @pragma('vm:prefer-inline')
+  @override
+  $Res call({
+    Object? id = freezed,
+    Object? name = freezed,
+    Object? language = freezed,
+    Object? avatarUrl = freezed,
+    Object? starCount = freezed,
+    Object? watchersCount = freezed,
+    Object? forksCount = freezed,
+    Object? openIssueCount = freezed,
+    Object? htmlUrl = freezed,
+  }) {
+    return _then(_value.copyWith(
+      id: freezed == id
+          ? _value.id
+          : id // ignore: cast_nullable_to_non_nullable
+              as int?,
+      name: freezed == name
+          ? _value.name
+          : name // ignore: cast_nullable_to_non_nullable
+              as String?,
+      language: freezed == language
+          ? _value.language
+          : language // ignore: cast_nullable_to_non_nullable
+              as String?,
+      avatarUrl: freezed == avatarUrl
+          ? _value.avatarUrl
+          : avatarUrl // ignore: cast_nullable_to_non_nullable
+              as String?,
+      starCount: freezed == starCount
+          ? _value.starCount
+          : starCount // ignore: cast_nullable_to_non_nullable
+              as int?,
+      watchersCount: freezed == watchersCount
+          ? _value.watchersCount
+          : watchersCount // ignore: cast_nullable_to_non_nullable
+              as int?,
+      forksCount: freezed == forksCount
+          ? _value.forksCount
+          : forksCount // ignore: cast_nullable_to_non_nullable
+              as int?,
+      openIssueCount: freezed == openIssueCount
+          ? _value.openIssueCount
+          : openIssueCount // ignore: cast_nullable_to_non_nullable
+              as int?,
+      htmlUrl: freezed == htmlUrl
+          ? _value.htmlUrl
+          : htmlUrl // ignore: cast_nullable_to_non_nullable
+              as String?,
+    ) as $Val);
+  }
+}
+
+/// @nodoc
+abstract class _$$RepositoryImplCopyWith<$Res>
+    implements $RepositoryCopyWith<$Res> {
+  factory _$$RepositoryImplCopyWith(
+          _$RepositoryImpl value, $Res Function(_$RepositoryImpl) then) =
+      __$$RepositoryImplCopyWithImpl<$Res>;
+  @override
+  @useResult
+  $Res call(
+      {int? id,
+      String? name,
+      String? language,
+      String? avatarUrl,
+      @JsonKey(name: 'stargazers_count') int? starCount,
+      @JsonKey(name: 'watchers_count') int? watchersCount,
+      @JsonKey(name: 'forks_count') int? forksCount,
+      @JsonKey(name: 'open_issue_count') int? openIssueCount,
+      @JsonKey(name: 'html_url') String? htmlUrl});
+}
+
+/// @nodoc
+class __$$RepositoryImplCopyWithImpl<$Res>
+    extends _$RepositoryCopyWithImpl<$Res, _$RepositoryImpl>
+    implements _$$RepositoryImplCopyWith<$Res> {
+  __$$RepositoryImplCopyWithImpl(
+      _$RepositoryImpl _value, $Res Function(_$RepositoryImpl) _then)
+      : super(_value, _then);
+
+  @pragma('vm:prefer-inline')
+  @override
+  $Res call({
+    Object? id = freezed,
+    Object? name = freezed,
+    Object? language = freezed,
+    Object? avatarUrl = freezed,
+    Object? starCount = freezed,
+    Object? watchersCount = freezed,
+    Object? forksCount = freezed,
+    Object? openIssueCount = freezed,
+    Object? htmlUrl = freezed,
+  }) {
+    return _then(_$RepositoryImpl(
+      id: freezed == id
+          ? _value.id
+          : id // ignore: cast_nullable_to_non_nullable
+              as int?,
+      name: freezed == name
+          ? _value.name
+          : name // ignore: cast_nullable_to_non_nullable
+              as String?,
+      language: freezed == language
+          ? _value.language
+          : language // ignore: cast_nullable_to_non_nullable
+              as String?,
+      avatarUrl: freezed == avatarUrl
+          ? _value.avatarUrl
+          : avatarUrl // ignore: cast_nullable_to_non_nullable
+              as String?,
+      starCount: freezed == starCount
+          ? _value.starCount
+          : starCount // ignore: cast_nullable_to_non_nullable
+              as int?,
+      watchersCount: freezed == watchersCount
+          ? _value.watchersCount
+          : watchersCount // ignore: cast_nullable_to_non_nullable
+              as int?,
+      forksCount: freezed == forksCount
+          ? _value.forksCount
+          : forksCount // ignore: cast_nullable_to_non_nullable
+              as int?,
+      openIssueCount: freezed == openIssueCount
+          ? _value.openIssueCount
+          : openIssueCount // ignore: cast_nullable_to_non_nullable
+              as int?,
+      htmlUrl: freezed == htmlUrl
+          ? _value.htmlUrl
+          : htmlUrl // ignore: cast_nullable_to_non_nullable
+              as String?,
+    ));
+  }
+}
+
+/// @nodoc
+@JsonSerializable()
+class _$RepositoryImpl implements _Repository {
+  const _$RepositoryImpl(
+      {this.id,
+      this.name,
+      this.language,
+      this.avatarUrl,
+      @JsonKey(name: 'stargazers_count') this.starCount,
+      @JsonKey(name: 'watchers_count') this.watchersCount,
+      @JsonKey(name: 'forks_count') this.forksCount,
+      @JsonKey(name: 'open_issue_count') this.openIssueCount,
+      @JsonKey(name: 'html_url') this.htmlUrl});
+
+  factory _$RepositoryImpl.fromJson(Map<String, dynamic> json) =>
+      _$$RepositoryImplFromJson(json);
+
+  @override
+  final int? id;
+  @override
+  final String? name;
+  @override
+  final String? language;
+  @override
+  final String? avatarUrl;
+  @override
+  @JsonKey(name: 'stargazers_count')
+  final int? starCount;
+  @override
+  @JsonKey(name: 'watchers_count')
+  final int? watchersCount;
+  @override
+  @JsonKey(name: 'forks_count')
+  final int? forksCount;
+  @override
+  @JsonKey(name: 'open_issue_count')
+  final int? openIssueCount;
+  @override
+  @JsonKey(name: 'html_url')
+  final String? htmlUrl;
+
+  @override
+  String toString() {
+    return 'Repository(id: $id, name: $name, language: $language, avatarUrl: $avatarUrl, starCount: $starCount, watchersCount: $watchersCount, forksCount: $forksCount, openIssueCount: $openIssueCount, htmlUrl: $htmlUrl)';
+  }
+
+  @override
+  bool operator ==(dynamic other) {
+    return identical(this, other) ||
+        (other.runtimeType == runtimeType &&
+            other is _$RepositoryImpl &&
+            (identical(other.id, id) || other.id == id) &&
+            (identical(other.name, name) || other.name == name) &&
+            (identical(other.language, language) ||
+                other.language == language) &&
+            (identical(other.avatarUrl, avatarUrl) ||
+                other.avatarUrl == avatarUrl) &&
+            (identical(other.starCount, starCount) ||
+                other.starCount == starCount) &&
+            (identical(other.watchersCount, watchersCount) ||
+                other.watchersCount == watchersCount) &&
+            (identical(other.forksCount, forksCount) ||
+                other.forksCount == forksCount) &&
+            (identical(other.openIssueCount, openIssueCount) ||
+                other.openIssueCount == openIssueCount) &&
+            (identical(other.htmlUrl, htmlUrl) || other.htmlUrl == htmlUrl));
+  }
+
+  @JsonKey(ignore: true)
+  @override
+  int get hashCode => Object.hash(runtimeType, id, name, language, avatarUrl,
+      starCount, watchersCount, forksCount, openIssueCount, htmlUrl);
+
+  @JsonKey(ignore: true)
+  @override
+  @pragma('vm:prefer-inline')
+  _$$RepositoryImplCopyWith<_$RepositoryImpl> get copyWith =>
+      __$$RepositoryImplCopyWithImpl<_$RepositoryImpl>(this, _$identity);
+
+  @override
+  Map<String, dynamic> toJson() {
+    return _$$RepositoryImplToJson(
+      this,
+    );
+  }
+}
+
+abstract class _Repository implements Repository {
+  const factory _Repository(
+      {final int? id,
+      final String? name,
+      final String? language,
+      final String? avatarUrl,
+      @JsonKey(name: 'stargazers_count') final int? starCount,
+      @JsonKey(name: 'watchers_count') final int? watchersCount,
+      @JsonKey(name: 'forks_count') final int? forksCount,
+      @JsonKey(name: 'open_issue_count') final int? openIssueCount,
+      @JsonKey(name: 'html_url') final String? htmlUrl}) = _$RepositoryImpl;
+
+  factory _Repository.fromJson(Map<String, dynamic> json) =
+      _$RepositoryImpl.fromJson;
+
+  @override
+  int? get id;
+  @override
+  String? get name;
+  @override
+  String? get language;
+  @override
+  String? get avatarUrl;
+  @override
+  @JsonKey(name: 'stargazers_count')
+  int? get starCount;
+  @override
+  @JsonKey(name: 'watchers_count')
+  int? get watchersCount;
+  @override
+  @JsonKey(name: 'forks_count')
+  int? get forksCount;
+  @override
+  @JsonKey(name: 'open_issue_count')
+  int? get openIssueCount;
+  @override
+  @JsonKey(name: 'html_url')
+  String? get htmlUrl;
+  @override
+  @JsonKey(ignore: true)
+  _$$RepositoryImplCopyWith<_$RepositoryImpl> get copyWith =>
+      throw _privateConstructorUsedError;
+}

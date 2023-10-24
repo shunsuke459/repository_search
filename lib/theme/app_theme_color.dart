17dc217f78ea5740c19d952b302b0634cc3b4f57
shunsuke459 <shunsuke459@gmail.com>
2023-10-25 01:41:52 +0900

    add: AppThemeColorに要素を追加

--- lib/theme/app_theme_color.dart
+++ lib/theme/app_theme_color.dart
@@ -6,6 +6,8 @@ enum AppThemeColor {
   red,
   text,
   primaryMain,
+  primaryDark,
+  grayBorder,
 }
 
 extension AppThemeColorExtension on AppThemeColor {
@@ -21,6 +23,10 @@ extension AppThemeColorExtension on AppThemeColor {
         return const Color(0xFF333333);
       case AppThemeColor.primaryMain:
         return const Color(0xFFAC9C6D);
+      case AppThemeColor.primaryDark:
+        return const Color(0xFF574E33);
+      case AppThemeColor.grayBorder:
+        return const Color(0xFFF0F0F0);
     }
   }
 }
